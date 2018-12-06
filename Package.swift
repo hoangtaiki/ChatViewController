04b1483b41e32e4987159e4d0860c79e37151ec6
Harry.Tran <duchoang.vp@gmail.com>
2018-12-06 14:41:37 +0700

    Add Package.swift to support Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  ChatViewController
+//
+//  Created by Hoangtaiki on 12/06/18.
+//  Copyright © 2018 toprating. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "ChatViewController"
+)
